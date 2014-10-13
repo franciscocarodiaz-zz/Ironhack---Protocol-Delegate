dd79d660ebb5db1318eeb6be85511695848344b3
Francisco Caro Diaz <franciscocarodiaz@gmail.com>
2014-10-13 12:56:06 +0200

    Initial Commit

---
+++ Beers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Beers
+//
+//  Created by Francisco Caro Diaz on 13/10/14.
+//  Copyright (c) 2014 ironhack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
